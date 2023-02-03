38a1297b60f22cfcbc28c05d9da7531514108f76
washington254 <103877612+washington254@users.noreply.github.com>
2023-02-03 10:00:32 +0300

    animetube

---
+++ animetube/app.js
@@ -0,0 +1,10 @@
+const menuIcon = document.querySelector(".menu-icon");
+const sideBar = document.querySelector(".sidebar");
+const containera = document.querySelector(".containera");
+
+menuIcon.addEventListener("click", () => {
+    sideBar.classList.toggle("small-sidebar");
+    containera.classList.toggle("large-container");
+});
+
+
