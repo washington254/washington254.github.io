5bf65b879741d243a786cdbebee63dc8dc8275f1
washington254 <103877612+washington254@users.noreply.github.com>
2023-04-10 15:34:05 +0300

    keems

---
+++ KEEMS/readme.txt
@@ -0,0 +1,8 @@
+hey there
+ 
+this is just a proof of concept the website has been inspired by LYNEE'S KEEMS WEBSITE
+so this is a way of thanking her
+ 
+i would like to credit the images to her too
+
+use this website as you like no licences no copyright just enjoy yourself
---
+++ KEEMS/script.js
@@ -0,0 +1,53 @@
+/*menu down JS query */
+var menuitems =document.getElementById("menu-items");
+
+menuitems.style.maxHeight ="0px";
+
+function menutoggle(){
+    if(menuitems.style.maxHeight =="0px")
+    {
+        menuitems.style.maxHeight ="200px";
+    }
+    else
+    {
+        menuitems.style.maxHeight ="0px";
+    }
+}
+
+/*product image JS query */
+var productimg =document.getElementById("product-img");
+var smallimg =document.getElementsByClassName("small-img");
+
+smallimg[0].onclick = function()
+{
+    productimg.src = smallimg[0].src;
+}
+smallimg[1].onclick = function()
+{
+    productimg.src = smallimg[1].src;
+}
+smallimg[2].onclick = function()
+{
+    productimg.src = smallimg[2].src;
+}
+smallimg[3].onclick = function()
+{
+    productimg.src = smallimg[3].src;
+}
+
+/*login reg page JS query */
+var loginform=document.getElementById("loginform");
+var regform=document.getElementById("regform");
+var indicator=document.getElementById("indicator");
+
+function register(){
+    regform.style.transform ="translateX(0px)";
+    loginform.style.transform ="translateX(0px)";
+    indicator.style.transform ="translateX(100px)";
+}
+
+function login(){
+    regform.style.transform ="translateX(300px)";
+    loginform.style.transform ="translateX(300px)";
+    indicator.style.transform ="translateX(0px)";
+}
