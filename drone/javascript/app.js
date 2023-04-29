b948ba22665a801a776aaf036a8d7f5dddb0a991
washington254 <103877612+washington254@users.noreply.github.com>
2023-04-29 13:21:38 +0300

    drone

---
+++ drone/javascript/app.js
@@ -0,0 +1,49 @@
+/*============ AUTO-TYPING-EFFECT========= */
+
+const typed = new Typed(".auto-type", {
+  strings: ["Innovative", "Aspiring", "Dedicated","X-TECHNOLOGIES"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true,
+});
+
+/*============ TOGGLE NAVBAR ========= */
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.addEventListener("click", ()=>{
+    menuIcon.classList.toggle("bx-x")
+    navbar.classList.toggle("active")
+})
+
+
+/*============ CHANGE ACTIVE NAVBAR ========= */
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach(section => {
+
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute("id");
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link => {
+                link.classList.remove("active");
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+            })
+        }
+    });
+    /*============ BOX-SHADOW NAVBAR ========= */
+    const header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*============ REMOVE NAVBAR AFTER LINK CLICK ========= */
+    menuIcon.classList.remove("bx-x")
+    navbar.classList.remove("active")
+
+};
+
